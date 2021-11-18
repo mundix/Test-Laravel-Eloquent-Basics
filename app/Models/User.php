78a2e7e1a0d8b67eba4691cfda87c0ba6a909ddd
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-11-18 11:52:18 -0400

    Completed: test_active_users

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'email_verified_at'
     ];
 
     /**
@@ -41,4 +42,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function scopeActive()
+    {
+        return User::whereNotNull('email_verified_at');
+
+    }
 }
